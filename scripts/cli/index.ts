1164b59cb3e69f3e4ed42b2bfbbd4f8066c67019
esoto76 <elpasocolorartist@yahoo.com>
2021-01-15 20:00:11 -0700

    feat: initialized scripts/cli out src/index.js

---
+++ scripts/cli/index.ts
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+import { Name } from "../configs/index";
+console.log(Name);
--- scripts/index.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import * as Pkg from "../package.json";
-
-export const Name: string = Pkg.name;
